9d5fcd11e860efaa56b25c9215e4d61face31996
darkushin <darkushin@paloaltonetworks.com>
2021-06-10 12:07:31 +0300

    Vector Space Model

--- src/webdata/ReviewSearch.java
+++ src/webdata/ReviewSearch.java
@@ -1,6 +1,7 @@
 package webdata;
 
 import java.util.*;
+import java.lang.Math.*;
 
 public class ReviewSearch {
     private IndexReader ir;
@@ -18,20 +19,38 @@ public class ReviewSearch {
      * The list should be sorted by the ranking
      */
     public Enumeration<Integer> vectorSpaceSearch(Enumeration<String> query, int k) {
-        return null;
+        // read entire query and compute query scores:
+        HashMap<String, Integer> fullQuery = new HashMap<>();
+        while (query.hasMoreElements()) {
+            String token = query.nextElement();
+            if (fullQuery.containsKey(token)){
+                fullQuery.put(token, fullQuery.get(token) + 1);
+            } else {
+                fullQuery.put(token, 1);
+            }
+        }
+        HashMap<String, Float> queryScores = this.computeTokenQueryScore(fullQuery);
+
+        HashMap<Integer, Double> scores= new HashMap<>();
+        for (String token: fullQuery.keySet()){
+            HashMap<Integer, Double> docScores = this.getDocScores(token, "vectorSpace");
+            double tokenQueryScore = queryScores.get(token);
+            for (int doc: docScores.keySet()){
+                double curScore = tokenQueryScore * docScores.get(doc);
+                if (scores.containsKey(doc)) {
+                    scores.put(doc, scores.get(doc) + curScore);
+                } else {
+                    scores.put(doc, curScore);
+                }
+            }
+        }
+        // sort the map and return the ids of the k highest scores:
+        return kHighestScores(scores, k);
     }
 
-    private HashMap<Integer, Double> getDocScores(String token) {
-        HashMap<Integer, Double> scores = new HashMap<>();
-        Enumeration<Integer> docFreqs = ir.getReviewsWithToken(token);
-        while (docFreqs.hasMoreElements()) {
-            int docId = docFreqs.nextElement();
-            int freq = docFreqs.nextElement();
-            double val = ((double) freq / ir.getReviewLength(docId));
-            scores.put(docId, val);
-        }
-        return scores;
-    }
+
+
+
 
     /**
      * Returns a list of the id-s of the k most highly ranked reviews for the
@@ -46,19 +65,13 @@ public class ReviewSearch {
             String token = query.nextElement();
             toks++;
             double smooth = (1 - lambda) * (double) ir.getTokenCollectionFrequency(token) / ir.getTokenSizeOfReviews();
-            HashMap<Integer, Double> tokenScores = getDocScores(token);
+            HashMap<Integer, Double> tokenScores = getDocScores(token, "languageModel");
             for (Map.Entry<Integer, Double> ent : tokenScores.entrySet()) {
                 double val = lambda * ent.getValue() + smooth;
                 scores.merge(ent.getKey(), Math.pow(val, toks), (x, y) -> x*val);
             }
         }
-        List<Map.Entry<Integer, Double>> list = new ArrayList<>(scores.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < Math.min(k, list.size()); i++) {
-            result.add(list.get(list.size() - i - 1).getKey());
-        }
-        return Collections.enumeration(result);
+        return kHighestScores(scores, k);
     }
 
     /**
@@ -68,6 +81,60 @@ public class ReviewSearch {
      */
     public Collection<String> productSearch(Enumeration<String> query, int k) {
         return null;
+    }
+
+    private HashMap<String, Float> computeTokenQueryScore(HashMap<String, Integer> query) {
+        HashMap<String, Float> scores = new HashMap<>();
+
+        // compute the tf and idf values of every token:
+        for (String token: query.keySet()) {
+            float tf = (float) (1 + Math.log10(query.get(token)));
+            float df = (float) Math.log10(ir.getNumberOfReviews() / ir.getTokenFrequency(token));
+            scores.put(token, tf*df);
+        }
+
+        // compute the norm of the vector:
+        double vectorNorm = 0;
+        for (double score: scores.values()){
+            vectorNorm += Math.pow(score, 2);
+        }
+
+        // normalize the values by dividing in the vector's norm:
+        for (String token: scores.keySet()){
+            float normalizedScore = (float) (scores.get(token) / vectorNorm);
+            scores.put(token, normalizedScore);
+        }
+        return scores;
+    }
+
+    private HashMap<Integer, Double> getDocScores(String token, String model) {
+        HashMap<Integer, Double> scores = new HashMap<>();
+        Enumeration<Integer> docFreqs = ir.getReviewsWithToken(token);
+        while (docFreqs.hasMoreElements()) {
+            int docId = docFreqs.nextElement();
+            int freq = docFreqs.nextElement();
+            double val = 0;
+            if (model.equals("languageModel")) {
+                val = ((double) freq / ir.getReviewLength(docId));
+            } else if (model.equals("vectorSpace")){
+                val = ((double) 1 + Math.log10(freq));
+            } else {
+                System.out.println("Please provide the name of the search for computing docScores. Options are: [languageModel, vectorSpace]");
+                System.exit(1);
+            }
+            scores.put(docId, val);
+        }
+        return scores;
+    }
+
+    private Enumeration<Integer> kHighestScores(HashMap<Integer, Double> scores, int k){
+        List<Map.Entry<Integer, Double>> list = new ArrayList<>(scores.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < Math.min(k, list.size()); i++) {
+            result.add(list.get(list.size() - i - 1).getKey());
+        }
+        return Collections.enumeration(result);
     }
 
     public static void main(String[] args) {
